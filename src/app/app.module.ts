666c0fa604cf499eae707fc055f202af4741eaed
Cyrille-Richard-74 <cyrillerichard.dev@gmail.com>
2023-01-17 17:50:21 +0100

    topbar-header-nav-footer added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './core/partials/footer/footer.component';
+import { HeaderComponent } from './core/partials/header/header.component';
+import { NavComponent } from './core/partials/nav/nav.component';
+import { TopbarComponent } from './core/partials/topbar/topbar.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TopbarComponent,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
