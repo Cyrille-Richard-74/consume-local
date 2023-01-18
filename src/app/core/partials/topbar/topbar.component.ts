97a552d16379c4f413b88e3b6a9b140135ac8a46
Cyrille-Richard-74 <cyrillerichard.dev@gmail.com>
2023-01-18 09:18:30 +0100

    topbar content added

--- src/app/core/partials/topbar/topbar.component.ts
+++ src/app/core/partials/topbar/topbar.component.ts
@@ -6,12 +6,33 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      topbar works!
-    </p>
+    <nav class="navbar navbar-dark bg-dark pt-3 pb-3">
+      <div class="container-fluid d-flex justify-content-evenly">
+        <div class="language">
+          <a class="navbar-brand" href="#">
+            <img src="/assets/images/flags/france-flag.png" alt="Choix de la langue française" width="24" height="20">
+          </a>
+          <a class="navbar-brand" href="#">
+            <img src="/assets/images/flags/united-kingdom-flag.png" alt="choix de la langue anglaise" width="24" height="20">
+          </a>
+        </div>
+        <div class="login d-flex">
+          <a class="navbar-brand d-flex align-items-center" href="#">
+            <span class="material-icons">account_circle</span>
+            <span class="header_login_title ms-2">Se connecter</span>
+          </a>  
+        </div>
+      </div>
+    </nav>
   `,
-  styles: [
-  ]
+  styles: [`
+    .material-icons, .header_login_title {
+      color: lightgrey;
+    }
+    .header_login_title {
+      font-size: 14px;
+    }
+  `]
 })
 export class TopbarComponent {
 
