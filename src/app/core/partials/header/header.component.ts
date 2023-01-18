64c3ba88f9a7fbfb932b58a9e0eb298bcf03e07f
Cyrille-Richard-74 <cyrillerichard.dev@gmail.com>
2023-01-18 09:53:09 +0100

    header content added

--- src/app/core/partials/header/header.component.ts
+++ src/app/core/partials/header/header.component.ts
@@ -6,12 +6,38 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      header works!
-    </p>
+      <div class="header pt-3">
+        <div class="favorite-cart d-flex justify-content-end me-5">
+          <div class="mt-2">
+            <button class="btn icon-button position-relative">
+              <span class="material-icons">favorite</span>
+              <span class="position-absolute top-0 start-90 translate-middle badge rounded-pill bg-danger mt-1">
+                10
+                <span class="visually-hidden">unread messages</span>
+              </span>
+            </button>
+            <button class="btn icon-button position-relative">
+              <span class="material-icons">shopping_cart</span>
+              <span class="position-absolute top-0 start-90 translate-middle badge rounded-pill bg-danger mt-1">
+                2
+                <span class="visually-hidden">unread messages</span>
+              </span>
+            </button>
+          </div>
+        </div>
+      </div>
+    
   `,
-  styles: [
-  ]
+  styles: [`
+  .material-icons {
+    font-size: 32px;
+    color: lightgrey;
+  }
+  .header {
+      height: 450px;
+      background-image: url("/assets/images/background/header_background.png");
+    }
+  `]
 })
 export class HeaderComponent {
 
